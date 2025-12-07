c728e084a0515de1b4c3da40059edf6f05e839f5
Rupa Devi <rupadevi7094@gmail.com>
2025-12-07 21:27:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const form = document.forms.form;
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+
+existingBtn.addEventListener("click", () => {
+	const credentials = JSON.parse(localStorage.getItem("credentials"));
+	alert(`logged in as ${credentials.username}`)
+})
+
+if(localStorage.getItem("credentials")){
+ existingBtn.removeAttribute("hidden")
+}
+	
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	alert(`Logged in as ${username.value}`)
+
+	let credentials = {}
+	if(checkbox.checked){
+		credentials = {username: username.value, password: password.value}
+		localStorage.setItem("credentials", JSON.stringify(credentials));
+		 existingBtn.removeAttribute("hidden")	
+	}
+	else{
+		localStorage.removeItem("credentials")
+		existingBtn.setAttribute("hidden", true)
+	}
+})
